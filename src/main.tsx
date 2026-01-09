d9e2461d2cf4b0cc2dc8c34b7d57028d9920738d
r36at <wyvern.claw@gmail.com>
2026-01-10 03:31:39 +0900

    更新:modern-normalizeをリセットCSSとして導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
+import 'modern-normalize/modern-normalize.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
