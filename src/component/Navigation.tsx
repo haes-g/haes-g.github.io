c593a1b63cf247afdc677284dc9662a3edfebe99
r36at <wyvern.claw@gmail.com>
2026-01-11 08:00:14 +0900

    修正:各回へのナビゲーションを追加

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -10,6 +10,9 @@ const Navigation = () => (
   <nav>
     <ul className='navigation'>
       <li><a href="/">ホーム</a></li>
+      <li><a href="https://sites.google.com/haes-g.info/homepage/%E7%AC%AC37%E5%9B%9E">第37回</a></li>
+      <li><a href="https://sites.google.com/haes-g.info/homepage/%E7%AC%AC38%E5%9B%9E">第38回</a></li>
+      <li><a href="https://sites.google.com/haes-g.info/homepage/%E7%AC%AC39%E5%9B%9E">第39回</a></li>
       <li><a href="/overview">概要</a></li>
       <li><a href="/history">沿革</a></li>
     </ul>
